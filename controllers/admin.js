97352d11be7674be5410b3f98d408474c0e3e1c0
tamisan <phongphat.jap@gmail.com>
2025-06-22 12:28:01 +0700

    udpate state

--- controllers/admin.js
+++ controllers/admin.js
@@ -19,3 +19,37 @@ exports.listStats = async (req, res, next) => {
     next(error);
   }
 };
+
+exports.listReservations = async (req, res, next) => {
+  try {
+    const { id } = req.user;
+    // 1.Count Campings
+    // 2.Count Nights
+    // 3.Totals
+
+    const campings = await prisma.landmark.count({
+      where: {
+        profileId: id,
+      },
+    });
+
+    const totals = await prisma.booking.aggregate({
+      where: {
+        profileId: id,
+      },
+      _sum: {
+        totalNights: true,
+        total: true,
+      },
+    });
+
+    console.log(totals);
+    res.json({
+      campings: campings,
+      nights: totals._sum.totalNights,
+      totals: totals._sum.total,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 // controllers
-const { listStats } = require("../controllers/admin.js");
+const { listStats, listReservations } = require("../controllers/admin.js");
 const { authCheck } = require("../middlewares/auth");
 
 // @ENDPOINT http://localhost:5000/api/stats
 router.get("/stats", authCheck, listStats);
+router.get("/reservations", authCheck, listReservations);
 
 module.exports = router;
